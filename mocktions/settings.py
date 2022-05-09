8604768bdabbe444e01be89299cf594d3b667952
Nathaniel <nhahoyt@gmail.com>
2022-05-09 16:28:19 -0500

    removed secret key

--- mocktions/settings.py
+++ mocktions/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*p+qoxi_b7g=&1b2s!a)x=f7c8t0lvn^4ydg63#)*c6m$=o*57'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
