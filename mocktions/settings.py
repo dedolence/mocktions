6f514709056a0339849088002a196e18523720d0
Nathaniel <nhahoyt@gmail.com>
2022-05-09 17:18:10 -0500

    gitignore in wrong directory?!

--- mocktions/settings.py
+++ mocktions/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = ''
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
