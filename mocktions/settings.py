5daa35a9073aba1fbd80e4f128a28571863692de
dedolence <nhahoyt@gmail.com>
2022-05-24 21:13:56 -0400

    staticfiles from now until eternity

--- mocktions/settings.py
+++ mocktions/settings.py
@@ -170,7 +170,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
