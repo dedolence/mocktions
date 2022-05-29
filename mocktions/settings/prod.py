889f3abd76aa721c26709014e99bf24eae868de0
dedolence <nhahoyt@gmail.com>
2022-05-28 22:03:53 -0400

    more staticfiles experiments

--- mocktions/settings/prod.py
+++ mocktions/settings/prod.py
@@ -5,7 +5,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['mocktions.herokuapp.com']
 
 # Sentry setup
