2ac09ce128b4457106b768464e18ff2724d3252d
dedolence <nhahoyt@gmail.com>
2022-05-29 14:13:54 -0400

    staticfiles hell

--- mocktions/settings/base.py
+++ mocktions/settings/base.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Additional locations to find  static files
-STATICFILES_DIR = [ os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIR = [BASE_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
