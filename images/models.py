f3a29925ced904d639f9fd0b4fba21e1c29b78a7
dedolence <nhahoyt@gmail.com>
2022-06-05 15:48:39 -0400

    Switching gears to work on user accounts.

--- images/models.py
+++ images/models.py
@@ -1,24 +1,7 @@
 from django.db import models
-import pathlib
-from uuid import uuid4
 
 # Adapted from this guide:
 # https://www.hacksoft.io/blog/direct-to-s3-file-upload-with-django
 
-def image_generate_upload_path(instance, filename):
-    return f"images/{instance.file_name}"
-
-""" class Image(models.Model):
-    image = models.ImageField(
-        upload_to=image_generate_upload_path,
-        null=True,
-        blank=True,
-    ) """
-
-def file_generate_name(original_file_name):
-    """
-        Generates unique file name but preserves file extension.
-    """
-    extension = pathlib.Path(original_file_name).suffix
-
-    return f"{uuid4().hex}{extension}"
+class ProfileImage(models.Model):
+    path = models.CharField(max_length=200, blank=False, null=False)
