1c0e7ff3b7c2236d4f5b032aee0dc574c7438475
dedolence <nhahoyt@gmail.com>
2022-11-28 14:42:57 -0500

    getting started with Selenium: added a simple login loop test

--- accounts/testing/selenium_tests.py
+++ accounts/testing/selenium_tests.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import LiveServerTestCase
 from django.urls import reverse_lazy
 from selenium import webdriver
@@ -7,32 +8,47 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from ..models import User
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 class LoginFormTest(LiveServerTestCase):
-    fixtures = ['user_fixtures.json']
+    """
+        Test the login loop:
+        - visit index,
+        - click on login, redirect,
+        - enter credentials (incorrectly),
+        - enter credentials (correctly),
+        - redirect to index
+    """
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.selenium = webdriver.Firefox()
-        cls.selenium.implicitly_wait(10)
+        cls.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        cls.driver.implicitly_wait(10)
+        cls.user = User.objects.create_user(
+            username="test_user",
+            password="test_password",
+        )
+        cls.user_id = cls.user.pk
 
-    """ @classmethod
+    @classmethod
     def tearDownClass(cls) -> None:
-        cls.selenium.quit()
-        super().tearDownClass() """
+        cls.driver.quit()
+        super().tearDownClass()
 
-    def test_form(self):
+    def test_visit_login(self):
+        # start at index, click on login
+        self.driver.get(self.live_server_url + reverse_lazy('base:index'))
+        login_link = self.driver.find_element(by=By.LINK_TEXT, value="Login")
+        login_link.click()
 
-        self.selenium.get('%s%s' % (self.live_server_url, reverse_lazy('accounts:login')))
+        username_input = self.driver.find_element(by=By.ID, value="id_username")
+        password_input = self.driver.find_element(by=By.ID, value="id_password")
+        submit_input = self.driver.find_element(by=By.CSS_SELECTOR, value="input[type='submit']")
 
-        username_input = self.selenium.find_element(by=By.ID, value="id_username")
-        password_input = self.selenium.find_element(by=By.ID, value="id_password")
-        submit_input = self.selenium.find_element(by=By.CSS_SELECTOR, value='input[type="submit"]')
+        username_input.send_keys('test_user')
+        password_input.send_keys('test_password')
+        submit_input.click()
 
-        username_input.send_keys("dedolence")
-        password_input.send_keys("chicken")
-        submit_input.send_keys(Keys.RETURN)
-
-        assert "/accounts/profile/1" in self.selenium.current_url
+        expected_url = self.live_server_url + reverse_lazy('accounts:profile', args=[self.user_id])
+        actual_url = self.driver.current_url    # FULL url including host 
+        self.assertEqual(actual_url, expected_url)
