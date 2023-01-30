bea096d93c485c593fd6728ec4fa555f22d9a662
dedolence <nhahoyt@gmail.com>
2023-01-30 13:08:35 -0500

    implement image models

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'base'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", include("django_backblaze_b2.urls")),
     path("images/", include("images.urls", namespace="images"))
 ]
