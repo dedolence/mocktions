56994b6332a64c0e7f3914b295f4b3f1c005ed45
dedolence <nhahoyt@gmail.com>
2023-02-20 11:38:31 -0500

    model admin not the solution i'm looking for

--- images/admin.py
+++ images/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
 from images.models import Image
-from typing import Any
-from django.http import HttpRequest
-
-class ImageAdmin(admin.ModelAdmin):
-
-    def save_model(self, request: Any, obj: Image, form: Any, change: Any) -> None:
-        obj.user = request.user
-        return super().save_model(request, obj, form, change)
-
-    def delete_model(self, request: HttpRequest, obj: Image) -> None:
-        return False
 
 admin.site.register(Image)
