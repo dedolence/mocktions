c722af1e5f667968e1f7bf6a2ef6cab24175068b
dedolence <nhahoyt@gmail.com>
2023-04-12 16:52:27 -0500

    clean up

--- images/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import CreateView
-from images.models import Image
-from django.http import HttpRequest, HttpResponse
-from typing import Any
-from django.shortcuts import render
-from images.test.test_form import TestForm
-from django.template.loader import render_to_string
-
